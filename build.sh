0f6b489c019ddc27fe788be2e8855f23fc949ddc
Jessica Kerr <jessitron@gmail.com>
2017-12-11 12:14:33 -0600

    fail

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 sleep 10
 cat monster.txt
 
+exit 1 #failinate!!
+
 # this is super secret Team City magic to tell a later step that this succeeded
 echo "##teamcity[setParameter name='secretmessage' value='Penguins LOL']"
