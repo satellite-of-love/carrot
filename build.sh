9ef0edc1c30c9350e50da07fc46c51bb927d258c
Emma Wilson <ewilson@nvidia.com>
2017-12-12 09:44:20 -0600

    breakin'

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 sleep 10
 cat monster.txt
 
-##exit 1 #failinate!! FIXING THE PENGUINS BECAUSE I WANNA SHOW IT WORK
+exit 1 #failinate!! dang penguins
 
 # this is super secret Team City magic to tell a later step that this succeeded
 echo "##teamcity[setParameter name='secretmessage' value='Penguins LOL']"
