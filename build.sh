8995de099f6f089790e3cb5393f4888e99973cba
Emma Wilson <ewilson@nvidia.com>
2017-12-29 11:30:28 -0600

    changin' the restart reason

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+restart_reason=$1
+
 sleep 10
 cat monster.txt
 
+echo $restart_reason
 ##exit 1 #failinate!! no more penguins
+
 
 # this is super secret Team City magic to tell a later step that this succeeded
 echo "##teamcity[setParameter name='secretmessage' value='Penguins LOL']"
